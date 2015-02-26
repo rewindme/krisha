9fe388f5dda9cf834978a6a39e2c36876fec2594
rewindme <rewind.me@me.com>
2015-02-27 01:14:01 +0900

    상세페이지 수정

---
+++ src/dm/10_qna.php
@@ -0,0 +1,57 @@
+<?php include '_header.php' ?>
+
+<table width="100%" border="0" cellpadding="0" cellspacing="0" style="table-layout:fixed">
+	<tr>
+		<td style="padding:25px 27px;border-bottom:1px solid #e8e8e8">
+			<span style="font-size:24px;font-weight:bold;color:#666;font-family:sans-serif">{nameOrder}님,</span>
+			<p style="font-size:14px;color:#757575;line-height:20px;font-family:sans-serif;margin-bottom:0;">크리샤를 이용해주셔서 감사합니다.<br>
+				고객님께서 문의하신 내용에 대한 답변입니다.
+			</p>
+		</td>
+	</tr>
+	<tr>
+		<td style="border-bottom:1px solid #e8e8e8">
+			<table width="100%" border="0" cellpadding="0" cellspacing="0" style="table-layout:fixed;border-collapse:collapse">
+				<tr>
+					<td width="113" style="padding:18px 0;color:#5cc4b9;font-size:16px;font-weight:bold;font-family:sans-serif;vertical-align:top;text-align:center;line-height:22px">질문 제목</td>
+					<td style="color:#666;font-size:14px;font-family:sans-serif;padding:18px 6px;line-height:22px;">어제 크리샤 세트를 구입했는데요, 상품이 불량인거 같습니다.</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+	<tr>
+		<td style="border-bottom:1px solid #e8e8e8">
+			<table width="100%" border="0" cellpadding="0" cellspacing="0" style="table-layout:fixed;border-collapse:collapse">
+				<tr>
+					<td width="113" style="padding:18px 0;color:#5cc4b9;font-size:16px;font-weight:bold;font-family:sans-serif;vertical-align:top;text-align:center;line-height:22px">질문 내용</td>
+					<td style="color:#666;font-size:14px;font-family:sans-serif;padding:18px 6px;line-height:22px;">21일날 받은 크리샤 세트입니다.<br>근데 어제 확인해보니 상품이 불량인거 같아서요.<br>확인 부탁드립니다.</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+	<tr>
+		<td style="border-bottom:1px solid #e8e8e8">
+			<table width="100%" border="0" cellpadding="0" cellspacing="0" style="table-layout:fixed;border-collapse:collapse">
+				<tr>
+					<td width="113" style="padding:18px 0;color:#f54500;font-size:16px;font-weight:bold;font-family:sans-serif;vertical-align:top;text-align:center;line-height:22px">답변 제목</td>
+					<td style="color:#666;font-size:14px;font-family:sans-serif;padding:18px 6px;line-height:22px;">어제 크리샤 세트를 구입했는데요, 상품이 불량인거 같습니다.</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+	<tr>
+		<td style="border-bottom:1px solid #e8e8e8">
+			<table width="100%" border="0" cellpadding="0" cellspacing="0" style="table-layout:fixed;border-collapse:collapse">
+				<tr>
+					<td width="113" style="padding:18px 0;color:#f54500;font-size:16px;font-weight:bold;font-family:sans-serif;vertical-align:top;text-align:center;line-height:22px">덥변 내용</td>
+					<td style="color:#666;font-size:14px;font-family:sans-serif;padding:18px 6px;line-height:22px;">21일날 받은 크리샤 세트입니다.<br>근데 어제 확인해보니 상품이 불량인거 같아서요.<br>확인 부탁드립니다.
+						<p style="color:#999;font-size:12px;font-family:sans-serif;line-height:20px;">기타 다른 문의사항이 잇으시면, 크리샤 고객센터로 연락주시면 신속하게 처리해드리겠습니다. <br>항상 저희 크리샤 쇼핑몰을 이용해주셔서 감사합니다.</p>
+					</td>
+				</tr>
+			</table>
+		</td>
+	</tr>
+	
+</table>
+
+<?php include '_footer.php' ?>
